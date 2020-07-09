6b93748d34ae241822a36bb8d079567bdb14a701
yuliia-beetann <chubykina96@gmail.com>
2020-07-09 23:07:22 +0300

    add navigation

--- src/components/Home/navigation/navigation.js
+++ src/components/Home/navigation/navigation.js
@@ -3,8 +3,15 @@ import './navigation.scss';
 
 function Navigation() {
   return (
-    <div>
-      Navigation
+    <div class="navigation">
+      <ul class="navigation__list">
+        <li class="navigation__item"><a href="#">restaurant</a></li>
+        <li class="navigation__item"><a href="#">Menu</a></li>
+        <li class="navigation__item"><a href="#">bakery</a></li>
+        <li class="navigation__item"><a href="#">Catering</a></li>
+        <li class="navigation__item"><a href="#">market&wines</a></li>
+        <li class="navigation__item"><a href="#">contact</a></li>
+      </ul>
     </div>
   );
 }
