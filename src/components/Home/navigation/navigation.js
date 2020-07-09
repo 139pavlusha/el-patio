cdb8d3f8286e48ab5087b4fc70cc1400037a4eb8
yuliia-beetann <chubykina96@gmail.com>
2020-07-09 23:11:30 +0300

    small change

--- src/components/Home/navigation/navigation.js
+++ src/components/Home/navigation/navigation.js
@@ -3,14 +3,14 @@ import './navigation.scss';
 
 function Navigation() {
   return (
-    <div class="navigation">
-      <ul class="navigation__list">
-        <li class="navigation__item"><a href="#">restaurant</a></li>
-        <li class="navigation__item"><a href="#">Menu</a></li>
-        <li class="navigation__item"><a href="#">bakery</a></li>
-        <li class="navigation__item"><a href="#">Catering</a></li>
-        <li class="navigation__item"><a href="#">market&wines</a></li>
-        <li class="navigation__item"><a href="#">contact</a></li>
+    <div className="navigation">
+      <ul className="navigation__list">
+        <li className="navigation__item"><a href="#">restaurant</a></li>
+        <li className="navigation__item"><a href="#">Menu</a></li>
+        <li className="navigation__item"><a href="#">bakery</a></li>
+        <li className="navigation__item"><a href="#">Catering</a></li>
+        <li className="navigation__item"><a href="#">market&wines</a></li>
+        <li className="navigation__item"><a href="#">contact</a></li>
       </ul>
     </div>
   );
