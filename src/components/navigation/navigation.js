ae8a00ac4ce0fb3ee624d1ad2d0b427cd90d3dfc
zordexxx <zordexxx69@gmail.com>
2020-07-12 13:54:49 +0300

    fa-facebook

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -26,18 +26,3 @@ export default class Navigation extends Component {
       );
     }
   }
-    
-  
-  
-  
-  
-  // <div className="navigation">
-  //   <ul className="navigation__list">
-  //     <li className="navigation__item"><a href="#">restaurant</a></li>
-  //     <li className="navigation__item"><a href="#">Menu</a></li>
-  //     <li className="navigation__item"><a href="#">bakery</a></li>
-  //     <li className="navigation__item"><a href="#">Catering</a></li>
-  //     <li className="navigation__item"><a href="#">market&wines</a></li>
-  //     <li className="navigation__item"><a href="#">contact</a></li>
-  //   </ul>
-  // </div>
