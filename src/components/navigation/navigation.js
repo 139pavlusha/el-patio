9d25333f56341066a628d760ec3878787bbe2871
139pavlusha <pavel1grek@gmail.com>
2020-07-14 17:57:43 +0300

    fixing after fixing

--- src/components/navigation/navigation.js
+++ src/components/navigation/navigation.js
@@ -3,7 +3,6 @@ import NavItem from "./nav-item/nav-item";
 import './navigation.scss';
 
 export default class Navigation extends Component {
-<<<<<<< HEAD
 
   menu = [
     { name: "restaurant", url: "/main", id: 1 },
@@ -27,28 +26,3 @@ export default class Navigation extends Component {
     );
   }
 }
-=======
-
-    menu = [
-      { name: "restaurant", url: "/main", id: 1 },
-      { name: "menu", url: "/menu/lunch/sandwiches", id: 2 },
-      { name: "bakery", url: "/bakery", id: 3 },
-      { name: "catering", url: "/catering", id: 4 },
-      { name: "market&wines", url: "/market", id: 5 },
-      { name: "contact", url: "/contact", id: 6 },
-    ];
-    render() {
-      return (
-       <div className="navigation">
-          <ul className="navigation__list">
-            {this.menu.map((item) => {
-              return (
-                <NavItem itemName={item.name} url={item.url} key={item.id} />
-              );
-            })}
-          </ul>
-        </div>
-      );
-    }
-  }
->>>>>>> e6a8516e0e14783c1d5052f430865db7034d8d9f
